86defbf3a6d79515c00bb1eb37b156e117328188
Mohit Chandak <65466004+mohitchandak@users.noreply.github.com>
2022-05-09 01:57:57 +0530

    Time: 31 ms (54.88%), Space: 19.1 MB (83.86%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,10 +1,40 @@
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//       int j=0;
+//         for(int i=0; i<nums.size(); i++){
+//             if(nums[i]!=0)
+//                 swap(nums[j++], nums[i]);
+//         }
+//     }
+// };
+
+
+
+// here we are calculation no of zero peresent in array 
+// and then appending in fcrom back . 
+
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-      int j=0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]!=0)
-                swap(nums[j++], nums[i]);
-        }
-    }
+        int count = 0, index = 0;
+       int n = nums.size();
+       for(int i = 0; i <nums.size(); i++) {
+           if(nums[i] == 0)
+               count++;
+           else {
+               nums[index] = nums[i];
+               index++;
+               
+           }
+       }
+       
+       while(count > 0) {
+           nums[--n] = 0;
+           count--;
+           
+       }
+	}
+        
 };
