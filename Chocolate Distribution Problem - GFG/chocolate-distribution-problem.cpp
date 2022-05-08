b7b0e9e26d8c118e51b91f4c29158c418b4da7cd
Mohit Chandak <65466004+mohitchandak@users.noreply.github.com>
2022-05-09 02:22:49 +0530

    Added solution - LeetHub

--- Chocolate Distribution Problem - GFG/chocolate-distribution-problem.cpp
+++ Chocolate Distribution Problem - GFG/chocolate-distribution-problem.cpp
@@ -7,18 +7,30 @@ class Solution{
     public:
     long long findMinDiff(vector<long long> a, long long n, long long m){
     //code
-   sort(a.begin(),a.end());
-   long long min_diff=a[a.size()-1]-a[0];
-   int i=0;
-   int j=i+m-1;
-   while(j<a.size())
-   {
-       min_diff=min(min_diff,(a[j]-a[i]));
-       i++;
-       j=i+m-1;
-   }
-   return min_diff;
-    }   
+   // Sort the given packets 
+        sort(a.begin(),a.end());
+		
+		long long start = 0,end =0;
+		// Largest number of chocolates 
+		long long mind = LONG_LONG_MAX;
+		
+		// Find the subarray of size m such that 
+        // difference between last (maximum in case 
+        // of sorted) and first (minimum in case of 
+        // sorted) elements of subarray is minimum.
+		for(long long i=0;i+m-1<n;i++)
+		{
+			long long diff = a[i+m-1] - a[i];
+			if(mind>diff)
+			{
+				mind = diff;
+				start = i;
+				end = i+m-1;
+			}
+		}
+		return a[end]-a[start];
+            
+    }
 };
 
 // { Driver Code Starts.
