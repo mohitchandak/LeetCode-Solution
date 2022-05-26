c5cc162823fe6d0f8cffbd5d564d676fd77c6042
Mohit Chandak <65466004+mohitchandak@users.noreply.github.com>
2022-05-26 23:02:14 +0530

    Time: 7 ms (12.48%), Space: 6 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        return __builtin_popcount(n);
+        int ans = 0;
+        while(n > 0)
+        {
+            n = n & (n-1);
+            ans++;;
+        }
+        return ans;
+    
     }
 };
+
+// class Solution {
+// public:
+//     int hammingWeight(uint32_t n) {
+//         //return __builtin_popcount(n); //returns number of set bits in n.
+//           }
+// };
